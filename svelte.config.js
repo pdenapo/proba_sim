0c6e28de37567373b869479b60d73f55a3c99610
Pablo De Nápoli <pdenapo@gmail.com>
2024-10-28 16:50:59 -0300

    usamos el adaptador auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 
@@ -8,12 +8,10 @@ const config = {
 	// Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
 	extensions: ['.svelte', '.md'],
 
-	preprocess: [
-		vitePreprocess(),
-	],
+	preprocess: [vitePreprocess()],
 
 	kit: {
-		adapter: adapter({fallback: "index.html"}),
+		adapter: adapter({ fallback: 'index.html' })
 	}
 };
 
