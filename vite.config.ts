db890e4907b4da25ebf52b3c269435b3508bef30
Pablo De Nápoli <pdenapo@gmail.com>
2025-02-26 13:56:03 -0300

    Cleanup and update depedencies. Removed vitest, playright, elint.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
 	resolve: {
 		alias: {
 		  '@': fileURLToPath(new URL('./src', import.meta.url))
